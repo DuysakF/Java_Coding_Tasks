0cadb97bf2dca6593c274b62a0c9f3b867386746
AuGrY <fduysakk@gmail.com>
2024-05-25 20:24:58 +0200

    EvenOddNumbers Task completed

---
+++ src/javacodingtasks/week1/EvenOddNumbers.java
@@ -0,0 +1,21 @@
+package javacodingtasks.week1;
+
+public class EvenOddNumbers {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 6;
+
+        if (num1 % 2 == 0) {
+            System.out.println(num1 + " is Even");
+        } else {
+            System.out.println(num1 + " is Odd");
+        }
+
+        if (num2 % 2 == 0) {
+            System.out.println(num2 + " is Even");
+        } else {
+            System.out.println(num2 + " is Odd");
+        }
+    }
+}
