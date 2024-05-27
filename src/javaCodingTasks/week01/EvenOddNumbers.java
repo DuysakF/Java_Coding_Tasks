53e37e7647ad78892474353adc874cde404ac0a7
Fatih <fatih.simsek@hotmail.de>
2024-05-27 16:47:01 +0200

    added method evenOddNumbers

--- src/javaCodingTasks/week01/EvenOddNumbers.java
+++ src/javaCodingTasks/week01/EvenOddNumbers.java
@@ -1,4 +1,12 @@
 package javaCodingTasks.week01;
 
 public class EvenOddNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(evenOddNumbers(10));
+    }
+
+    public static String evenOddNumbers(int n) {
+        return (n % 2 == 0) ? "even" : "odd";
+    }
 }
