edf6a7a6420d1f974fd094dd5b07003c78624f5e
Mehmet Mertcan <mehmetmertcande@gmail.com>
2024-05-28 15:42:13 +0200

    Some changes made. Task as comment included.

--- src/java_coding_tasks/week01/EvenOddNumbers.java
+++ src/java_coding_tasks/week01/EvenOddNumbers.java
@@ -2,3 +2,15 @@ package java_coding_tasks.week01;
 
 public class EvenOddNumbers {
 }
+
+/*
+
+Write a method which can identifies given number is even or odd
+
+Ex:
+
+identify(5) -> "Odd"
+identify(6) -> "Even"
+
+ */
+
