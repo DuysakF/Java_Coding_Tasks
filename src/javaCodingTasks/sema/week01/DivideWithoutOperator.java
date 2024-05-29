87cdafc63a507ed4a81d30235b449b90b1411543
sema <se.sari4242@gmail.com>
2024-05-29 16:10:52 +0200

    gitignore

---
+++ src/javaCodingTasks/sema/week01/DivideWithoutOperator.java
@@ -0,0 +1,25 @@
+package javaCodingTasks.sema.week01;
+
+public class DivideWithoutOperator {
+    public static void main(String[] args) {
+
+            int bolunen =12;
+            int bolen=3;
+            int count=0;
+            if (bolen==0){
+                System.out.println("Gecersiz islem");
+            }
+            while (bolunen>=bolen){
+                int sonuc=bolunen-bolen;
+                bolunen-=bolen;
+                count++;
+
+
+            }
+            System.out.println(count);
+            System.out.println(bolunen);
+
+        }
+    }
+
+
---
+++ src/javaCodingTasks/sema/week01/EvenOddNumbers.java
@@ -0,0 +1,17 @@
+package javaCodingTasks.sema.week01;
+
+public class EvenOddNumbers {
+    public static void main(String[] args) {
+        oddEven(8);
+        oddEven(3);
+
+    }
+    public static void oddEven(int num){
+        if(num%2==0){
+            System.out.println("The number is Odd");
+        }else {
+            System.out.println("The number is Even");
+        }
+
+    }
+}
