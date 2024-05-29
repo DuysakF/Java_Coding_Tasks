2d61863a5b5469ac671f81a9aeefa308d1cc46ca
Hamide-P <hamide.pehlivan@hotmail.com>
2024-05-29 20:15:55 +0200

    Tasks added

--- src/javaCodingTasks/week01/EvenOddNumbers.java
+++ src/javaCodingTasks/week01/EvenOddNumbers.java
@@ -1,4 +1,31 @@
 package javaCodingTasks.week01;
 
 public class EvenOddNumbers {
-}
+
+
+    public static void main(String[] args) {
+
+        System.out.println(evenOrOdd(5));
+        System.out.println(evenOrOdd(6));
+
+    }
+
+    public static String evenOrOdd(int num) {
+        return (num % 2 == 0) ? num + " is even number" : num + " is odd number ";
+    }
+
+
+    }
+
+
+/*
+Write a method which can identifies given number is even or odd
+
+Ex:
+identify(5) -> "Odd"
+identify(6) -> "Even"
+*/
+
+
+
+
