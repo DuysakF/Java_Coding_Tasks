a29145c6d5fa0914dba1dc1ab22941db5a47c162
cengizcicek <serife.sancar@gmail.com>
2024-06-01 15:37:59 +0200

    update

--- src/javaCodingTasks/week01/serife/FinRa.java
+++ src/javaCodingTasks/week01/serife/FinRa.java
@@ -13,7 +13,7 @@ public class FinRa {
     public static void _FinRa() {
 
         for (int i = 1; i <= 30; i++) {
-            if( i %  3 == 0 & i % 5 == 0){
+            if( i %  3 == 0 && i % 5 == 0){
                 System.out.print(" FINRA " );
             } else if (i % 3 == 0) {
                 System.out.print(" FIN ");
