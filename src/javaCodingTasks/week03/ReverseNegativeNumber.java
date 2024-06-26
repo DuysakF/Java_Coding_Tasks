1ece5a8c785eda3b0b70aab5ed1e61ae7c6af369
Fatih <fatih.simsek@hotmail.de>
2024-06-26 10:51:37 +0200

    ReverseNegativeNumber.java edited

--- src/javaCodingTasks/week03/ReverseNegativeNumber.java
+++ src/javaCodingTasks/week03/ReverseNegativeNumber.java
@@ -1,31 +1,4 @@
 package javaCodingTasks.week03;
-<<<<<<< HEAD
-
-public class ReverseNegativeNumber {
-
-    // Method to reverse a negative number
-    public static int reverseNegative(int number) {
-
-        // Convert the negative number to a string
-        String numStr = Integer.toString(number);
-
-        // Reverse the string except the negative sign
-        String reversedStr = new StringBuilder(numStr.substring(1)).reverse().toString();
-
-        // Add the negative sign back and parse the result as an integer
-        int reversedNum = Integer.parseInt("-" + reversedStr);
-
-        return reversedNum;
-    }
-
-    public static void main(String[] args) {
-        int num = -663; // Example negative number
-        int reversedNum = reverseNegative(num);
-
-        System.out.println("Original number: " + num);
-        System.out.println("Reversed number: " + reversedNum);
-    }
-=======
 public class ReverseNegativeNumber {
 
     public static void main(String[] args) {
@@ -57,5 +30,4 @@ public class ReverseNegativeNumber {
     }
 
 
->>>>>>> origin/fatihs
 }
